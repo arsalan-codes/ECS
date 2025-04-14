c6a33f065e9a724f9cda65bc5e082814f3c7d92d
A <arsalanrezazadeh4@gmail.com>
2025-04-14 20:35:52 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button variant="outline" size="icon">
    
    with the text content:
    
        Toggle theme
    
    Relevant files:
    
        - https://6000-idx-studio-1744650537119.cluster-fnjdffmttjhy2qqdugh3yehhs2.cloudworkstations.dev/_next/static/chunks/src_117d15b6._.js
        - https://6000-idx-studio-1744650537119.cluster-fnjdffmttjhy2qqdugh3yehhs2.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
        - https://6000-idx-studio-1744650537119.cluster-fnjdffmttjhy2qqdugh3yehhs2.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    remove this

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -266,11 +266,6 @@ export default function Home() {
             <CardTitle> 📊 ECS</CardTitle>
             <CardDescription>Environmental Control System</CardDescription>
           </div>
-            <Button variant="outline" size="icon" onClick={toggleTheme}>
-                {theme ? <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"/> :
-                    <Moon className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"/>}
-                <span className="sr-only">Toggle theme</span>
-            </Button>
         </CardHeader>
         <CardContent>
           <AnimatedBarChart value={avgTemperature} label="Average Temperature" color="hsl(var(--chart-1))"/>
@@ -448,3 +443,4 @@ export default function Home() {
     </div>
   );
 }
+
